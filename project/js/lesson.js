014286a63bfcc89bfcbe10ade42a57deae84e864
doni <dgumasevdaniel@gmail.com>
2026-02-16 18:36:46 +0600

    Add files via upload

--- project/js/lesson.js
+++ project/js/lesson.js
@@ -45,3 +45,41 @@ const conventer = (element) => {
 conventer(somInput);
 conventer(usdInput);
 conventer(eurInput);
+
+
+// Card Switcher
+
+const card = document.querySelector('.card');
+const btnNext = document.querySelector('#btn-next');
+const btnPrev = document.querySelector('#btn-prev');
+let cardId = 1;
+
+function loadCard(id) {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            const { title, id, completed } = data;
+            const compliteTitle = completed ? 'Выполнено' : 'Не выполнено';
+            const compliteColor = completed ? 'green' : 'red';
+
+            card.innerHTML = `
+                <p>${title}</p>
+                <p style="color: ${compliteColor}">${compliteTitle}</p>
+                <span>${id}</span>
+            `;
+        });
+}
+
+btnNext.onclick = () => {
+    cardId++;
+    if (cardId > 200) cardId = 1;
+    loadCard(cardId);
+}
+
+btnPrev.onclick = () => {
+    cardId--;
+    if (cardId < 1) cardId = 200;
+    loadCard(cardId);
+}
+
+loadCard(cardId);
